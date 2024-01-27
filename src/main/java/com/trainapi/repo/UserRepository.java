2aa2969195281c708369f49e39c822e48891fc3e
girishu-ms26 <girishums1999@gmail.com>
2024-01-27 13:33:00 +0530

    removed unused method

--- src/main/java/com/trainapi/repo/UserRepository.java
+++ src/main/java/com/trainapi/repo/UserRepository.java
@@ -4,9 +4,8 @@ import com.trainapi.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-    List<User> findUsersBySeat_SeatIdIn(List<Integer> seatId);
+
 }
