271e149be462917d303a07f30b5099d6b57dd9ed
girishu-ms26 <girishums1999@gmail.com>
2024-04-13 17:23:09 +0530

    Added CrossOrigin for react application

--- src/main/java/com/trainapi/controller/TrainController.java
+++ src/main/java/com/trainapi/controller/TrainController.java
@@ -5,6 +5,7 @@ import com.trainapi.service.TrainService;
 import com.trainapi.constants.TrainApiConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(TrainApiConstants.BASE_PATH)
+@CrossOrigin(origins = "http://localhost:3000")
 public class TrainController {
 
     private final TrainService trainService;
